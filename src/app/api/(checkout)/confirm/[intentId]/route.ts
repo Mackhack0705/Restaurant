a4b1d915627a4114dc6e19627b9bba35323a6fec
Rajesh Mali <rajeshmali@cyntexa.com>
2024-03-30 21:16:51 +0530

    Changed Put method presentation

--- src/app/api/(checkout)/confirm/[intentId]/route.ts
+++ src/app/api/(checkout)/confirm/[intentId]/route.ts
@@ -1,25 +1,26 @@
 import { prisma } from "@/utils/connect";
-import { NextResponse } from "next/server";
+import { NextApiRequest, NextApiResponse } from "next";
 
-export const PUT = async ({ params }: { params: { intentId: string } }) => {
-  const { intentId } = params;
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method === "PUT") {
+    const { intentId } = req.query;
 
-  try {
-    await prisma.order.update({
-      where: {
-        intent_id: intentId,
-      },
-      data: { status: "Being prepared!" },
-    });
-    return new NextResponse(
-      JSON.stringify({ message: "Order has been updated" }),
-      { status: 200 }
-    );
-  } catch (err) {
-    console.log(err);
-    return new NextResponse(
-      JSON.stringify({ message: "Something went wrong!" }),
-      { status: 500 }
-    );
+    try {
+      await prisma.order.update({
+        where: {
+          intent_id: intentId as string,
+        },
+        data: { status: "Being prepared!" },
+      });
+      res.status(200).json({ message: "Order has been updated" });
+    } catch (err) {
+      console.error(err);
+      res.status(500).json({ message: "Something went wrong!" });
+    }
+  } else {
+    res.status(405).json({ message: "Method Not Allowed" });
   }
-};
+}
